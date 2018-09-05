0e7f5b173fa5502b40bf9206821497b5510fa3fc
ConSilverstone <ConnorJamesGallagher@outlook.com>
2018-09-05 14:38:44 +0100

    Added library stdlib.h and a system pause.

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,16 @@
+// Include Libraries
+#include <stdlib.h> //standard library
+
 // Entry point for our program
 // Every C++ program needs a main() function
 
 int main()
 {
+
+	//Thei system function will wait for the user th press a button before continuing
+	system("pause");
+
+
 	//Tell the operating system we are done
 	//and there was no error
 	return 0;
