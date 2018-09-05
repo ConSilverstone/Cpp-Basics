733a3efffbe43822b4dd45dee8dc225820346dd3
ConSilverstone <ConnorJamesGallagher@outlook.com>
2018-09-05 14:44:58 +0100

    Added am output printing text.

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,14 @@
 // Include Libraries
 #include <stdlib.h> //standard library
+#include <iostream> //input and output library
 
 // Entry point for our program
 // Every C++ program needs a main() function
 
 int main()
 {
+
+	std::cout << "Hello World!" << std::endl;//Prints the text to display on screen
 
 	//Thei system function will wait for the user th press a button before continuing
 	system("pause");
